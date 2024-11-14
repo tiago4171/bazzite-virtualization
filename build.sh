5317b373b86ad1f9825024e2b42c89df1f02a476
Tiago J. Silva <tiagojsvla@gmail.com>
2024-11-14 20:04:47 -0300

    Update build.sh with virtualization packages and some extras

--- build.sh
+++ build.sh
@@ -12,12 +12,16 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# this install extra packages to monitor hardware performance
+rpm-ostree install tmux btop iotop sysstat
+
+# Install @virtualization group of packages
+rpm-ostree install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer virt-top
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
+systemctl enable libvirtd.service
