243ed779c0de6201c6ddfc8eef59b66ca845d4a7
Tiago J. Silva <tiagojsvla@gmail.com>
2024-11-15 13:07:02 -0300

    Update build.sh to try to fix extras not being installed

--- build.sh
+++ build.sh
@@ -12,16 +12,8 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this install extra packages to monitor hardware performance
-rpm-ostree install tmux btop iotop sysstat
-
-# Install @virtualization group of packages
-rpm-ostree install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer virt-top
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
+# this install extra packages to monitor hardware performance and @virtualization packages
+rpm-ostree install tmux btop iotop sysstat libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-manager virt-viewer virt-top
 
 #### Example for enabling a System Unit File
-
-#systemctl enable podman.socket
 systemctl enable libvirtd.service
